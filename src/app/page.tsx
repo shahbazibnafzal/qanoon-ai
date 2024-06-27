cd294d673e2426cc3c745e1639ca3ded56b13f7b
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2024-06-27 23:16:41 +0200

    Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function HomePage() {
-  return <main></main>;
+  return (
+    <main className="flex justify-center py-4 ">
+      <div>
+        <h1 className="text text-center text-3xl font-bold">Qanoon AI</h1>
+        <p>Your AI assistant for legal queries is currently studying law. </p>
+      </div>
+    </main>
+  );
 }
